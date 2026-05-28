77ddd3f44d8127b53d9284bb5d334a54544c2076
Xrojim <rojemmaharjan@gmail.com>
2026-05-28 22:26:15 +0545

    feat: configure Django settings

--- movie_recommendation/settings.py
+++ movie_recommendation/settings.py
@@ -239,5 +239,3 @@ TMDB_API_BASE_URL = 'https://api.themoviedb.org/3'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-#for later
